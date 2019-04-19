ca46f885bc41ec196b384d337e162a67db6512c1
arpanmangal <mangalarpan@gmail.com>
2019-04-19 14:55:03 +0530

    Importing TA changes

--- lab3_io.cu
+++ lab3_io.cu
@@ -15,29 +15,17 @@ void read_matrix (const char* input_filename, int* M, int* N, double** D){
 	fclose(fin);
 }
 
-
-void printMatrix (double *M, int m, int n) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%f ", M[i*n + j]);
-        }
-        printf("\n");
-    }
-    printf("-----------\n");
-}
-
 void write_result (int M, 
 		int N, 
 		double* D, 
 		double* U, 
 		double* SIGMA, 
 		double* V_T,
+		int SIGMAm, 
+		int SIGMAn, 
 		int K, 
 		double* D_HAT,
 		double computation_time){
 	// Will contain output code
-	// printMatrix (U, N, N);
-    printMatrix (SIGMA, 1, N);
-	printf("\n%d %d %d\n\n", M, N, K);
-	printMatrix (D_HAT, M, K);
+
 }
